2e0539209bf2ecfc24be855bac0fa975c182f285
Tasneem Burmawala <tasneemhburmawala@gmail.com>
2021-01-18 20:15:52 +0000

    add endpoint for sending html

--- express_server.js
+++ express_server.js
@@ -12,6 +12,9 @@ app.get("/", (req, res)=>{
 app.get("/urls.json", (req, res)=>{
   res.json(urlDatabase);
 });
+app.get("/hello", (req, res)=>{
+  res.send("<html><body>Hello <b>World</b></body></html>");
+});
 app.listen(PORT, ()=>{
   console.log(`Example listening on port ${PORT}`);
 });
