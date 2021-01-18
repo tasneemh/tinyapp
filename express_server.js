8ddec84f554d24272614cf40796746ecf842dc77
Tasneem Burmawala <tasneemhburmawala@gmail.com>
2021-01-18 19:57:57 +0000

    add additional endpoint for urlDatabase

--- express_server.js
+++ express_server.js
@@ -9,6 +9,9 @@ const urlDatabase = {
 app.get("/", (req, res)=>{
   res.send("Hello!");
 });
+app.get("/urls.json", (req, res)=>{
+  res.json(urlDatabase);
+});
 app.listen(PORT, ()=>{
   console.log(`Example listening on port ${PORT}`);
 });
