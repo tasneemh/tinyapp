5879d69acd5149fbdb8194e89b2ba849d69d11cf
Tasneem Burmawala <tasneemhburmawala@gmail.com>
2021-01-19 00:10:22 +0000

    Adding a post route to receive form submission

--- express_server.js
+++ express_server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
-
+//below code helps in using body-parser
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
 //below code tells express to use ejs as templating engine
 app.set("view engine", "ejs");
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
 app.get("/", (req, res)=>{
   res.send("Hello!");
 });
@@ -31,12 +34,23 @@ app.get("/urls", (req, res)=>{
   const templateVars = {urls: urlDatabase};
   res.render("urls_index", templateVars);
 });
+//Add a GET Route to Show the Form
+app.get("/urls/new", (req, res)=>{
+  res.render("urls_new");
+});
 app.get("/urls/:shortURL", (req, res)=>{
   const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
   // console.log(req.params.shortURL); //shortURL: req.params.shortURL
   // console.log(urlDatabase[req.params.shortURL]); // longURL: urlDatabase[shortURL]
 });
+app.post("/urls", (req, res)=>{
+  console.log(req.body);
+  res.send("Ok");
+});
 app.listen(PORT, ()=>{
   console.log(`Example listening on port ${PORT}`);
 });
+const generateRandomString = function() {
+  return Math.random().toString(36).slice(2, 8);
+};
